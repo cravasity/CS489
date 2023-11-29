30bf4997999fcc8bc7e5bfc983a31f0a27010513
juhyunlee <dhwndudkaps2@kaist.ac.kr>
2023-11-29 13:10:50 +0900

    clean: delete unnecessary codes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,6 @@ import Rank from './Rank'
 import Testing from './Testing'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// function App() {
-//   return (-
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// jsx file에서 image load하기
 function App() {
   return (
     <div className="App">
